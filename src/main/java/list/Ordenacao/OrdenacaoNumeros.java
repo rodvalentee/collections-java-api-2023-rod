ea38c6b26811adf73f112193ce136ea107f697e1
rodvalentee <rodrigovalente.ti@gmail.com>
2023-09-20 01:04:39 -0300

    Exercicios List

---
+++ src/main/java/list/Ordenacao/OrdenacaoNumeros.java
@@ -0,0 +1,72 @@
+package main.java.list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoNumeros {
+
+    List<Integer> numerosList;
+
+    public OrdenacaoNumeros() {
+        this.numerosList = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero){
+        this.numerosList.add(numero);
+    }
+
+    public List<Integer> ordenarAscendente(){
+        List<Integer> numerosAscendente = new ArrayList<>(this.numerosList);
+        if(!numerosAscendente.isEmpty()){
+            Collections.sort(numerosAscendente);
+            return numerosAscendente;
+        } else {
+            throw new RuntimeException("Lista vazia!");
+        }
+    }
+    public List<Integer> ordenarDescendente(){
+        List<Integer> numerosDescendente = new ArrayList<>(this.numerosList);
+        if(!numerosDescendente.isEmpty()){
+            numerosDescendente.sort(Collections.reverseOrder());
+            return numerosDescendente;
+        } else {
+            throw new RuntimeException("Lista vazia!");
+        }
+    }
+
+    public void exibirNumeros() {
+        if (!numerosList.isEmpty()) {
+            System.out.println(this.numerosList);
+        } else {
+            System.out.println("Lista vazia!");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Criando uma instância da classe OrdenacaoNumeros
+        OrdenacaoNumeros numeros = new OrdenacaoNumeros();
+
+        // Adicionando números à lista
+        numeros.adicionarNumero(2);
+        numeros.adicionarNumero(5);
+        numeros.adicionarNumero(4);
+        numeros.adicionarNumero(1);
+        numeros.adicionarNumero(99);
+
+        // Exibindo a lista de números adicionados
+        numeros.exibirNumeros();
+
+        // Ordenando e exibindo em ordem ascendente
+        System.out.println(numeros.ordenarAscendente());
+
+        // Exibindo a lista
+        numeros.exibirNumeros();
+
+        // Ordenando e exibindo em ordem descendente
+        System.out.println(numeros.ordenarDescendente());
+
+        // Exibindo a lista
+        numeros.exibirNumeros();
+    }
+}
